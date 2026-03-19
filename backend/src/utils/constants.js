e56cc5a5a39955cd184cc0f54c620c23e4705b88
Harshit <harshitsetia.005@gmail.com>
2026-03-19 16:35:58 +0530

    add utility functions for password hashing and JWT token generation

--- backend/src/utils/constants.js
+++ backend/src/utils/constants.js
@@ -1,8 +1,14 @@
 import "dotenv/config"
 
+// port
 export const PORT=process.env.PORT
+
+// DB
 export const DB_NAME=process.env.DB_NAME
 export const DB_USER=process.env.DB_USER
 export const DB_PASSWORD=process.env.DB_PASSWORD
 export const DB_HOST=process.env.DB_HOST
 export const DB_DIALECT=process.env.DB_DIALECT
+
+// JWT
+export const JWT_SECRET=process.env.JWT_SECRET
